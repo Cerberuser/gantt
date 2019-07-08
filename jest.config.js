8fca414f0c9286cb4782927470b6fb53724a0961
k.bagrov <k.bagrov@g.nsu.ru>
2019-07-08 18:02:10 +0700

    Fixed config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     transform: {
-        '^.+\\.(t|j)sx?$': 'ts-jest'
+        '^.+\\.tsx?$': 'ts-jest'
     },
     moduleNameMapper: {
         '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js'
