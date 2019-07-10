f6887b2fda334387a89523e78e27017566f77de7
k.bagrov <k.bagrov@g.nsu.ru>
2019-07-10 16:34:07 +0700

    Mocked getBBox SVG function; test is passing

--- tests/snapshots.test.ts
+++ tests/snapshots.test.ts
@@ -2,6 +2,16 @@
 
 import { Gantt, IOptions, ITask } from '../src';
 
+beforeAll(
+    () =>
+        ((SVGElement as any).prototype.getBBox = () => ({
+            height: 100,
+            width: 100,
+            x: 0,
+            y: 0
+        }))
+);
+
 function render(tasks: ITask[], options: Partial<IOptions>) {
     document.body.innerHTML = '<div id="container"></div>';
     const container: Element = document.querySelector('#container')!;
@@ -23,7 +33,7 @@ describe('Gantt renderer', () => {
                     id: 'id1',
                     name: 'name1',
                     start: '2019-01-01',
-                    end: '2019-12-31'
+                    end: '2019-01-02'
                 }
             ],
             {}
